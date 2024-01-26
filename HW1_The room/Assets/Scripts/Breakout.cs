8c89e0df7899737a2df94804016959bd137915c7
VINCENT Gabriel <vincentgab78@gmail.com>
2024-01-26 17:33:51 +0200

    Breakout script corrected

--- HW1_The room/Assets/Scripts/Breakout.cs
+++ HW1_The room/Assets/Scripts/Breakout.cs
@@ -24,13 +24,13 @@ public class Breakout : MonoBehaviour
             }
             else if (transform.position == new Vector3(0, 12.9f, -5.3f))
             {
-                transform.Translate(new Vector3(-14.7f, 15f, 19.4f));
-                transform.Rotate(new Vector3(0, 135, 0));
+                transform.Translate(new Vector3(-14.7f, -0.9f, 24.7f));
+                transform.Rotate(new Vector3(0, 135f, 0));
             }
-            else if (transform.position == new Vector3(-14.7f, 15f, 19.4f))
+            else if (transform.position == new Vector3(-14.7f, 12f, 19.4f))
             {
-                transform.Translate(new Vector3(0, 0, 0));
-                transform.Rotate(new Vector3(0, 0, 0));
+                transform.Rotate(new Vector3(0, 225f, 0));
+                transform.Translate(new Vector3(14.7f, -12f, -19.4f));
             }
         };
     }
